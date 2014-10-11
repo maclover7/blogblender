cd7e6eafa941795c5667d1ccb3763f07d694c3e3
Jon Moss <me@jonathanmoss.me>
2014-10-11 10:43:49 -0400

    Comment associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009235908) do
+ActiveRecord::Schema.define(version: 20141011144253) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "entry_id"
+  end
+
+  add_index "comments", ["entry_id"], name: "index_comments_on_entry_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "communities", force: true do |t|
     t.string   "name"
